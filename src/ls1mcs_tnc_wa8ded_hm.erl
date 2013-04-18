8ad4395f675d6b0d892d027b6beeda03fcd1c93b
Karolis Petrauskas <k.petrauskas@gmail.com>
2013-04-18 21:55:21 +0300

    Code cleanup.

--- src/ls1mcs_tnc_wa8ded_hm.erl
+++ src/ls1mcs_tnc_wa8ded_hm.erl
@@ -9,16 +9,17 @@
 -compile([{parse_transform, lager_transform}]).
 -behavour(gen_server).
 -behaviour(ls1mcs_protocol).
--export([start_link/3, send/2, received/2]). % Public API
+-export([start_link/4, send/2, received/2]). % Public API
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 %%
 %%  See ASCII for more details.
 %%
--define(DC1, 16#11).
--define(CAN, 16#18).
--define(ESC, 16#1B).
--define(CR,  16#0D).
+-define(DC1,    16#11).
+-define(CAN,    16#18).
+-define(ESC,    16#1B).
+-define(CR,     16#0D).
+-define(SPACE,  16#20).
 
 -define(HM_CHANNEL0, 0).
 -define(HM_CHANNEL1, 1).
@@ -34,9 +35,13 @@
 -define(HM_CODE_CON_INFO,   7). % Connected information     byte-count format
 
 -define(UART_OPTIONS, [{baud, 9600}, {csize, 8}, {parity, none}, {mode, binary}]).
--define(RESTART_DELAY, 1000).   % Milliseconds to sleep before connecting to the UART.
--define(QUERY_DELAY, 1000).     % Milliseconds.
--define(RESYNC_LEN, 300).       % Just because 300 > 256.
+-define(RESTART_DELAY, 1000).       % Milliseconds to sleep before connecting to the UART.
+-define(QUERY_DELAY, 1000).         % Milliseconds.
+-define(RESYNC_ATTEMPTS, 300).      % Just because 300 > 256.
+-define(RESYNC_DELAY, 100).         % Time to wait for response when performing HM sync.
+-define(BLOCK_READ_TIMEOUT, 1000).  % Timeout used when reading strings.
+-define(COMMAND_TIMEOUT, 5000).     % Time to wait for a command response.
+
 
 
 %% =============================================================================
@@ -49,11 +54,11 @@
 %%  TODO: Add some TNC commands to this module. e.g. quit hostmode, reset.
 %%  TODO: Set own call.
 %%
-%%  ls1mcs_tnc_wa8ded_hm:start_link({n, l, test}, {ls1mcs_tnc_wa8ded_hm, {n, l, test}}, "/dev/ttyUSB0").
+%%  ls1mcs_tnc_wa8ded_hm:start_link({n, l, test}, {ls1mcs_tnc_wa8ded_hm, {n, l, test}}, "/dev/ttyUSB0", "LY2EN").
 %%  ls1mcs_tnc_wa8ded_hm:send({n, l, test}, <<"Hello world!">>).
 %%
-start_link(Name, Upper, Device) ->
-    gen_server:start_link({via, gproc, Name}, ?MODULE, {Upper, Device}, []).
+start_link(Name, Upper, Device, LocalCall) ->
+    gen_server:start_link({via, gproc, Name}, ?MODULE, {Upper, Device, LocalCall}, []).
 
 
 %%
@@ -66,7 +71,8 @@ send(Ref, Data) when is_binary(Data) ->
 %%
 %%
 %%
-received(_Ref, _data) ->
+received(_Ref, Data) ->
+    lager:error("received/2 is not supported in this module, but ~p received.", [Data]),
     undefined.
 
 
@@ -78,6 +84,7 @@ received(_Ref, _data) ->
 -record(state, {
     upper,      %% Upper protocol layer.
     device,     %% Device we are working with (/dev/ttyUSB0)
+    local_call, %% Local call (HAM)
     port        %% UART port.
 }).
 
@@ -90,9 +97,9 @@ received(_Ref, _data) ->
 %%
 %%
 %%
-init({Upper, Device}) ->
+init({Upper, Device, LocalCall}) ->
     self() ! {initialize},
-    {ok, #state{upper = Upper, device = Device}}.
+    {ok, #state{upper = Upper, device = Device, local_call = LocalCall}}.
 
 
 %%
@@ -116,13 +123,15 @@ handle_cast(_Msg, State) ->
 %%
 %%  Initialize COM port and sync with the TNC.
 %%
-handle_info({initialize}, State = #state{upper = Upper, device = Device}) ->
+handle_info({initialize}, State = #state{upper = Upper, device = Device, local_call = LocalCall}) ->
     ls1mcs_protocol:await(Upper),
     lager:debug("Upper protocol started"),
 
     ok = receive after ?RESTART_DELAY -> ok end,
     {ok, Port} = uart:open(Device, ?UART_OPTIONS),
     ok = enter_hostmode(Port),
+    ok = set_local_call(Port, LocalCall),
+
     self() ! {query_input},
     {noreply, State#state{port = Port}};
 
@@ -163,7 +172,7 @@ read_all(Port) ->
     {ok, list_to_binary(Input)}.
 
 read_all(Port, List) ->
-    case uart:recv(Port, 1, 1000) of
+    case uart:recv(Port, 1, ?BLOCK_READ_TIMEOUT) of
         {ok, <<Byte:8>>} -> read_all(Port, [Byte | List]);
         {error, timeout} -> lists:reverse(List)
     end.
@@ -177,7 +186,7 @@ read_zstr(Port) ->
     {ok, list_to_binary(Input)}.
 
 read_zstr(Port, List) ->
-    case uart:recv(Port, 1, 1000) of
+    case uart:recv(Port, 1, ?BLOCK_READ_TIMEOUT) of
         {ok, <<0:8>>} -> lists:reverse(List);
         {ok, <<Byte:8>>} -> read_zstr(Port, [Byte | List]);
         {error, timeout} -> {error, timeout}
@@ -188,9 +197,9 @@ read_zstr(Port, List) ->
 %%  Reads length-prefixed data.
 %%
 read_lstr(Port) ->
-    case uart:recv(Port, 1, 1000) of
+    case uart:recv(Port, 1, ?BLOCK_READ_TIMEOUT) of
         {ok, <<ByteCount:8>>} ->
-            case uart:recv(Port, ByteCount, 1000) of
+            case uart:recv(Port, ByteCount, ?BLOCK_READ_TIMEOUT) of
                 {ok, Message} -> {ok, Message};
                 {error, timeout} -> {error, timeout}
             end;
@@ -203,18 +212,18 @@ read_lstr(Port) ->
 %%  Executes hostmode resync procedure: sends 1's till error is received.
 %%
 resync_hostmode(Port) ->
-    resync_hostmode(Port, ?RESYNC_LEN).
+    resync_hostmode(Port, ?RESYNC_ATTEMPTS).
 
 resync_hostmode(_Port, 0) ->
     {error, unable_to_sync};
 
 resync_hostmode(Port, Count) when Count > 0 ->
     ok = uart:send(Port, <<1>>),
-    case uart:recv(Port, 1, 100) of
+    case uart:recv(Port, 1, ?RESYNC_DELAY) of
         {ok, <<?HM_CHANNEL1>>} ->
             % Assume <<1>> is channel number. Now we expect of <<2>> -- error indicator.
             % If <<2>> received, read zero-ended error message and assume sync'ed state.
-            case uart:recv(Port, 1, 100) of
+            case uart:recv(Port, 1, ?RESYNC_DELAY) of
                 {ok, <<?HM_CODE_FAILURE>>} ->
                     {ok, ErrMsg} = read_zstr(Port),
                     lager:debug("Expexted error ~p received, synced!", [ErrMsg]),
@@ -239,7 +248,7 @@ resync_hostmode(Port, Count) when Count > 0 ->
 send_cmd(Port, Command) when is_binary(Command) ->
     Count = size(Command) - 1,
     ok = uart:send(Port, <<?HM_CHANNEL0, ?HM_CMD, Count, Command/binary>>),
-    {ok, <<?HM_CHANNEL0, Code>>} = uart:recv(Port, 2, 5000),
+    {ok, <<?HM_CHANNEL0, Code>>} = uart:recv(Port, 2, ?COMMAND_TIMEOUT),
     {ok, Code}.
 
 
@@ -250,7 +259,7 @@ send_cmd(Port, Command) when is_binary(Command) ->
 send_info(Port, Data) when is_binary(Data) ->
     Count = size(Data) - 1,
     ok = uart:send(Port, <<?HM_CHANNEL0, ?HM_INFO, Count, Data/binary>>),
-    {ok, <<?HM_CHANNEL0, ?HM_CODE_OK>>} = uart:recv(Port, 2, 5000),
+    {ok, <<?HM_CHANNEL0, ?HM_CODE_OK>>} = uart:recv(Port, 2, ?COMMAND_TIMEOUT),
     ok.
 
 
@@ -260,6 +269,8 @@ send_info(Port, Data) when is_binary(Data) ->
 %%  3. Try to resync in hostmode.
 %%
 determine_mode(Port) ->
+    lager:debug("Determining TNC mode..."),
+
     {ok, Trash} = read_all(Port),
     lager:debug("Trash read from the TNC: ~p.", [Trash]),
 
@@ -268,7 +279,7 @@ determine_mode(Port) ->
     lager:debug("Command mode request responded with: ~p.", [EscResponse]),
 
     case lists:reverse(binary_to_list(EscResponse)) of
-        [32, $* | _] ->
+        [?SPACE, $* | _] ->
             lager:debug("Determined, its terminal mode!"),
             {ok, terminal};
         _ ->
@@ -308,23 +319,23 @@ query_all_input(Port, Upper) ->
             ok;
         {ok, ?HM_CODE_OK_MSG} ->
             {ok, Msg} = read_zstr(Port),
-            lager:warning("G responded OK with message ~p", Msg),
+            lager:warning("G responded OK with message ~p", [Msg]),
             ok;
         {ok, ?HM_CODE_FAILURE} ->
             {ok, Msg} = read_zstr(Port),
-            lager:error("G failed with message ~p", Msg),
+            lager:error("G failed with message ~p", [Msg]),
             {error, {failure, Msg}};
         {ok, ?HM_CODE_STATUS} ->
             {ok, Msg} = read_zstr(Port),
-            lager:info("G responded with status message ~p", Msg),
+            lager:info("G responded with status message ~p", [Msg]),
             query_all_input(Port, Upper);
         {ok, ?HM_CODE_MHDR} ->
             {ok, Msg} = read_zstr(Port),
-            lager:debug("G responded with monitored header ~p", Msg),
+            lager:debug("G responded with monitored header ~p", [Msg]),
             query_all_input(Port, Upper);
         {ok, ?HM_CODE_MHDR_MSG} ->
             {ok, Msg} = read_zstr(Port),
-            lager:debug("G responded with monitored header ~p, info follows", Msg),
+            lager:debug("G responded with monitored header ~p, info follows", [Msg]),
             query_info(Port, Upper)
     end.
 
@@ -332,12 +343,34 @@ query_info(Port, Upper) ->
     case send_cmd(Port, <<"G">>) of
         {ok, ?HM_CODE_MON_INFO} ->
             {ok, Message} = read_lstr(Port),
-            lager:debug("Monitored info received: ~p", Message),
+            lager:debug("Monitored info received: ~p", [Message]),
             ok = ls1mcs_protocol:received(Upper, Message);
         {ok, ?HM_CODE_CON_INFO} ->
             {ok, Message} = read_lstr(Port),
-            lager:warn("Connected info received: ~p", Message),
+            lager:warn("Connected info received: ~p", [Message]),
             ok = ls1mcs_protocol:received(Upper, Message)
     end.
+
+
+%%
+%%  Set local call for the TNC.
+%%
+set_local_call(Port, Call) ->
+    CallBin = list_to_binary(Call),
+    case send_cmd(Port, <<"I ", CallBin/binary>>) of
+        {ok, ?HM_CODE_OK} ->
+            lager:info("Local call set to ~p", [CallBin]),
+            ok;
+        {ok, ?HM_CODE_OK_MSG} ->
+            {ok, Msg} = read_zstr(Port),
+            lager:info("Local call set to ~p, message=~p", [CallBin, Msg]),
+            ok;
+        {ok, ?HM_CODE_FAILURE} ->
+            {ok, Msg} = read_zstr(Port),
+            lager:error("Unable to set call to ~p, error=~p", [CallBin, Msg]),
+            {error, {failure, Msg}}
+    end.
+
+
 
 
