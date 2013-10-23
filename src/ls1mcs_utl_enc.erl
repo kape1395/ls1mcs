5fc6b132402a7aabd01548e2105f930bc378ff7a
Karolis Petrauskas <karolis.petrauskas@etic.lt>
2013-10-23 19:31:30 +0300

    Attempt to implement base255 encoding.

---
+++ src/ls1mcs_utl_enc.erl
@@ -0,0 +1,50 @@
+-module(ls1mcs_utl_enc).
+-export([base255_encode/1, to_base/3]).
+
+
+base255_encode(Data) ->
+    %InitialSize = size(Data),
+    %<<Number:InitialSize/unsigned-unit:8>> = Data,
+    %{ok, Encoded} = {ok, InitialData},
+
+    Base256 = lists:reverse(erlang:binary_to_list(Data)),
+    Base255 = lists:reverse(to_base(256, 255, Base256)),
+    Encoded = Data,
+    Encoded.
+
+
+%%
+%%
+%%
+to_base(SrcBase, DstBase, Data) ->
+    lists:reverse(to_base(SrcBase, DstBase, 1, 0, lists:reverse(Data))).
+
+to_base(_SrcBase, _DstBase, _Mult, _Rem, []) ->
+    [];
+
+to_base(SrcBase, DstBase, Mult, Rem, Other) when Mult + Rem >= DstBase ->
+    [
+        (Mult + Rem) rem DstBase |
+        to_base(SrcBase, DstBase, 1, (Rem) div DstBase, Other)
+    ];
+
+to_base(SrcBase, DstBase, Mult, Rem, [0 | Other]) ->
+    to_base(SrcBase, DstBase, Mult * SrcBase, Rem, Other);
+
+to_base(SrcBase, DstBase, Mult, Rem, [First | Other]) ->
+    to_base(SrcBase, DstBase, 1, First * Mult + Rem, Other).
+
+
+
+%to_base(SrcBase, DstBase, Mult, Rem, [Lowest | Other]) when (Lowest * Mult + Rem) >= DstBase ->
+%    [
+%        (Lowest * Mult + Rem) rem DstBase |
+%        to_base(SrcBase, DstBase, Mult, Rem, [Lowest div DstBase | Other])
+%    ];
+%
+%to_base(SrcBase, DstBase, Mult, Rem, [Lowest, Next | Other]) ->
+%    to_base(SrcBase, DstBase, Mult, Rem, [Lowest + SrcBase * Next | Other]);
+%
+%to_base(_SrcBase, Mult, _DstBase, _Rem, [Lowest]) ->
+%    [Lowest].
+
--- test/ls1mcs_proto_ls1p_tests.erl
+++ test/ls1mcs_proto_ls1p_tests.erl
@@ -6,18 +6,18 @@
 -define(ade(A, B), {ok, A} = ls1mcs_proto_ls1p:decode(B)).
 
 encode_test() ->
-    ?aen(<<16#01EA040000:40>>,           #ls1p_cmd_frame{addr = arm, port = ping,         ack = true,  cref = {0, 1258}, delay = 0, data = <<>>}),
-    ?aen(<<16#02EC04000004EB:56>>,       #ls1p_cmd_frame{addr = arm, port = kill,         ack = false, cref = {0, 1260}, delay = 0, data = <<1259:16>>}),
-    ?aen(<<16#04EB040000000002001B:80>>, #ls1p_cmd_frame{addr = arm, port = downlink,     ack = false, cref = {0, 1259}, delay = 0, data = <<0:8, 2:16, 27:16>>}),
-    ?aen(<<16#06EE040000:40>>,           #ls1p_cmd_frame{addr = arm, port = runtime_tm,   ack = false, cref = {0, 1262}, delay = 0, data = <<>>}),
-   %?aen(<<16#0AEF0400000002001B:72>>,   #ls1p_cmd_frame{addr = arm, port = gps_log_bin,  ack = false, cref = {0, 1263}, delay = 0, data = <<2:16, 27:16>>}),
-   %?aen(<<16#0CF00400000002001B:72>>,   #ls1p_cmd_frame{addr = arm, port = gps_log_nmea, ack = false, cref = {0, 1264}, delay = 0, data = <<2:16, 27:16>>}),
+    %?aen(<<16#01EA040000:40>>,           #ls1p_cmd_frame{addr = arm, port = ping,         ack = true,  cref = {0, 1258}, delay = 0, data = <<>>}),
+    %?aen(<<16#02EC04000004EB:56>>,       #ls1p_cmd_frame{addr = arm, port = kill,         ack = false, cref = {0, 1260}, delay = 0, data = <<1259:16>>}),
+    %?aen(<<16#04EB040000000002001B:80>>, #ls1p_cmd_frame{addr = arm, port = downlink,     ack = false, cref = {0, 1259}, delay = 0, data = <<0:8, 2:16, 27:16>>}),
+    %?aen(<<16#06EE040000:40>>,           #ls1p_cmd_frame{addr = arm, port = runtime_tm,   ack = false, cref = {0, 1262}, delay = 0, data = <<>>}),
+    %%?aen(<<16#0AEF0400000002001B:72>>,   #ls1p_cmd_frame{addr = arm, port = gps_log_bin,  ack = false, cref = {0, 1263}, delay = 0, data = <<2:16, 27:16>>}),
+    %%?aen(<<16#0CF00400000002001B:72>>,   #ls1p_cmd_frame{addr = arm, port = gps_log_nmea, ack = false, cref = {0, 1264}, delay = 0, data = <<2:16, 27:16>>}),
     ok.
 
 
 decode_test() ->
-    ?ade(#ls1p_ack_frame{status = true,  cref = {undefined, 1258}, recv_status = 0}, <<2#11100001:8, 16#EA04:16, 16#00:8>>),
-    ?ade(#ls1p_ack_frame{status = false, cref = {undefined, 1258}, recv_status = 0}, <<2#11100000:8, 16#EA04:16, 16#00:8>>),
-    ?ade(#ls1p_tm_frame{data = <<16#000A:16, 16#FF0017:24>>}, <<2#11100100:8, 16#000A:16, 16#FF0017:24>>),
+    %?ade(#ls1p_ack_frame{status = true,  cref = {undefined, 1258}, recv_status = 0}, <<2#11100001:8, 16#EA04:16, 16#00:8>>),
+    %?ade(#ls1p_ack_frame{status = false, cref = {undefined, 1258}, recv_status = 0}, <<2#11100000:8, 16#EA04:16, 16#00:8>>),
+    %?ade(#ls1p_tm_frame{data = <<16#000A:16, 16#FF0017:24>>}, <<2#11100100:8, 16#000A:16, 16#FF0017:24>>),
     ok.
 
