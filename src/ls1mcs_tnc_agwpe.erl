461d5ad7600c4347462c12b9c2f3816b2f021627
Karolis Petrauskas <k.petrauskas@gmail.com>
2014-04-09 21:08:03 +0300

    Some notes on AGWPE based Soundmodem interface.

---
+++ src/ls1mcs_tnc_agwpe.erl
@@ -0,0 +1,15 @@
+-module(ls1mcs_tnc_agwpe).
+
+%%
+%%  [Čia](http://www.pe0sat.vgnet.nl/decoding/satellite-telemetry/sound-card-modem/) rašo,
+%%  kad reikia jungtis prie tcp 127.0.0.1 port 8000. Pats soundmodemas ir jo aprašymas yra
+%%  [čia](http://uz7.ho.ua/packetradio.htm). Kaip suprantu Soundmodemas naudoja AGW programos
+%%  api, tad detaliau pats sąsajos aprašymas yra čia: http://uz7.ho.ua/includes/agwpeapi.htm
+%%
+%%  Turime susintaliavę ir tą AGW programą ir išbandžiau, bet soundmodemas pasirodė
+%%  paprastesnis ir jautresnis.
+%%
+%%  Geriausi linkėjimai / Best Regards,
+%%  Laurynas M
+%%
+
