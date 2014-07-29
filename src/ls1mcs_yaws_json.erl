bf5cf20b5c9fe9c82c28efbf1d65ddbca88252f0
Karolis Petrauskas <k.petrauskas@gmail.com>
2014-07-29 20:53:15 +0300

    Some licence headers.

--- src/ls1mcs_yaws_json.erl
+++ src/ls1mcs_yaws_json.erl
@@ -1,3 +1,19 @@
+%/--------------------------------------------------------------------
+%| Copyright 2013-2014 Karolis Petrauskas
+%|
+%| Licensed under the Apache License, Version 2.0 (the "License");
+%| you may not use this file except in compliance with the License.
+%| You may obtain a copy of the License at
+%|
+%|     http://www.apache.org/licenses/LICENSE-2.0
+%|
+%| Unless required by applicable law or agreed to in writing, software
+%| distributed under the License is distributed on an "AS IS" BASIS,
+%| WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%| See the License for the specific language governing permissions and
+%| limitations under the License.
+%\--------------------------------------------------------------------
+
 -module(ls1mcs_yaws_json).
 -export([encode/1, encode_self/2, encode_list/1, encode_tstamp/1, decode/2, decode_atom/1, decode_integer/1]).
 -include("ls1mcs.hrl").
